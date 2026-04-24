3e8a1163b2e805d2dc0f975cd258b0acb96a7d62
dalilasignorelli <dalilasignorelli22@gmail.com>
2026-04-24 16:26:50 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,32 @@
-import {createFileRoute} from "@tanstack/react-router";
-import {redirect} from "@tanstack/router-core";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { HomePage } from './-components/HomePage'
 
 export const Route = createFileRoute("/")({
-  component: HomePage,
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  const navigate = useNavigate()
+
+  const handleNavigate = (page: 'home' | 'services' | 'about' | 'contact') => {
+    switch (page) {
+      case 'home':
+        navigate({ to: '/' })
+        break
+      case 'services':
+        navigate({ to: '/services' })
+        break
+      case 'about':
+        navigate({ to: '/about' })
+        break
+      case 'contact':
+        navigate({ to: '/contact' })
+        break
+    }
+  }
+  return (
+    <>
+      <HomePage onNavigate={handleNavigate}/>
+    </>
+  )
+}
