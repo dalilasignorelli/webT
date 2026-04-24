69224e1cca3615eac8af5deba2387fbea0a32878
dalilasignorelli <dalilasignorelli22@gmail.com>
2026-04-24 16:23:55 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,23 +2,5 @@ import {createFileRoute} from "@tanstack/react-router";
 import {redirect} from "@tanstack/router-core";
 
 export const Route = createFileRoute("/")({
-	component: App,
-    beforeLoad: async () => {
-    throw redirect({ to: "/homepage" });
-  }
+  component: HomePage,
 });
-
-function App() {
-  return (
-    <div className="p-4">
-      <header>
-        <h1 className="text-4xl font-bold">Homepage</h1>
-      </header>
-      <main>
-        <p>Benvenuto nella homepage dell'app!</p>
-      </main>
-    </div>
-  );
-}
-
-export default App;
